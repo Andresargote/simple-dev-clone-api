e199b9fbb4bdfca356d8e5dd6d785993694639a4
Andres <andresargote2018@gmail.com>
2021-07-16 21:26:13 +0200

    Agregado CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const passport = require("passport");
+const cors = require("cors");
 
 const { dbConnection } = require("./database/config");
 const app = express();
@@ -9,7 +10,9 @@ const { userRouter } = require("./api/resources/users/users.routes");
 const jwtStrategy = require("./api/auth/auth");
 const config = require("./config");
 
+
 passport.use(jwtStrategy);
+app.use(cors());
 app.use(express.json());
 
 dbConnection();
